a25f65272a842362349040f75ff6dba2139b56e7
Patrick Purviance <0191896@cheneysd.org>
2017-12-01 16:17:00 -0800

    Add project files.

---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public void docalc(double n1, double n2, int op)
+        {
+            result = 0;
+            ResetText();
+            switch (op)
+            {
+                case 1:
+                    result = n1 + n2;
+                    break;
+                case 2:
+                    result = n1 - n2;
+                    break;
+                case 3:
+                    result = n1 * n2;
+                    break;
+                case 4:
+                    result = n1 / n2;
+                    break;
+            }
+        }
+
+        public void ifequal()
+        {
+            double num2 = double.Parse(textBox1.Text);
+            docalc(num1, num2, opMain);
+            isfirst = false;
+        }
+
+        public void setText(string text)
+        {
+            if (text == "clear")
+            {
+                textBox1.Text = "";
+            }
+            else
+            {
+                if (issecond)
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            setText("1");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+        
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button0_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+
+        }
+        
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
